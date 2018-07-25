1f32b1754f969c66ada91de53bed8b021beb6d1f
Fanilus <fanil@demolit.ru>
2018-07-25 18:57:49 +0400

    Done work)

--- setup.sh
+++ setup.sh
@@ -1,23 +1,46 @@
+#!/bin/bash
+. fan_lib.sh
 # ++++++++++++ Setup eth server script by fan +++++++++++
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+
 # COMMANDS
-arr=("sudo apt-get install software-properties-common")
-arr+=('sudo add-apt-repository -y ppa:ethereum/ethereum')
-arr+=('sudo apt-get update')
-arr+=('sudo apt-get install ethereum')
+arr1=()
+#arr+=("sudo apt-get install software-properties-common")
+#arr+=('sudo add-apt-repository -y ppa:ethereum/ethereum')
+#arr+=('sudo apt-get update')
+#arr+=('sudo apt-get install ethereum')
+arr1+=('sudo apt-get install supervisor')
+arr1+=('sudo service supervisor restart')
 
-for (( i = 0; i < ${#arr[@]} ; i++ )); do
-    printf "\n***** Running: ${arr[$i]} *****\n"
-    # Run each command in array 
-    eval "${arr[$i]}" && echo "${green}[DONE]${reset} - ${arr[$i]}";
+HOME=`eval echo "~$different_user"`
+ETH_DATA_DIR='ethdata'
+IPADDR_DEF=`hostname --ip-address`
 
-    ### using back-ticks works also
-    #RESULT=`${arr[$i]}`
-    ### Check if the command gave any output
-    #if [ -n "$RESULT" ]; then
-    #    echo "$RESULT"
-    #fi
-done
-echo "${green}[DONE] - All ${#arr[@]} commands complete ${reset}"
+read -p "Please enter ip address for GETH [$IPADDR_DEF]:" ipaddr
+if [ "$ipaddr" = "" ]; then	
+	ipaddr=$IPADDR_DEF
+fi
+echo "ip address for GETH:$ipaddr"
+
+mkdir -p "$HOME/$ETH_DATA_DIR";
+
+echo "geth --rinkeby --networkid 4 --rpc --rpcaddr $ipaddr --rpcapi 'admin,debug,miner,shh,txpool,personal,eth,net,web3'  --datadir '$HOME/$ETH_DATA_DIR' --syncmode 'light' console" > "$HOME/geth.sh";
+
+echo "geth attach http://$ipaddr:8545" > "$HOME/attach.sh";
+
+#SUPERVISOR
+sudo bash -c 'echo "[program:geth]" > "/etc/supervisor/conf.d/geth.conf"';
+sudo bash -c 'echo "command=sh $HOME/geth.sh" >> "/etc/supervisor/conf.d/geth.conf"';
+sudo bash -c 'echo "autostart=true" >> "/etc/supervisor/conf.d/geth.conf"';
+sudo bash -c 'echo "autorestart=true" >> "/etc/supervisor/conf.d/geth.conf"';
+
+arr2=()
+arr2+=('sudo supervisorctl reread')
+arr2+=('sudo supervisorctl update')
+doCommands "${arr2[@]}"
+
+#doCommands "${arr1[@]}"
+
+
+# create  geth 
+
+
